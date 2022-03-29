7228b05b7a17a2f7f4e5254f91a30fc46d2b5e77
Omer Faruk Kilicaslan <kilicaslanomerkuf@gmail.com>
2022-03-29 11:18:16 +0300

    Initial Commit

---
+++ KNOWEATHERUITests/KNOWEATHERUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  KNOWEATHERUITestsLaunchTests.swift
+//  KNOWEATHERUITests
+//
+//  Created by Ömer Faruk Kılıçaslan on 29.03.2022.
+//
+
+import XCTest
+
+class KNOWEATHERUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
